bfec7e291c9793f21339c54597b39e51cc0ae0de
gifederspiel <gian.fedrespiel@bluewin.ch>
2022-06-03 11:26:33 +0200

    changes

--- src/routers/addWord.router.ts
+++ src/routers/addWord.router.ts
@@ -1,10 +1,23 @@
 import express from "express";
-import prisma from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 let router = express.Router();
+const prisma = new PrismaClient()
 
 router.get('/', async (req, res) => {
-    res.render('pool');
+    let numberOfWords = await (await prisma.wort.findMany({})).length
+    console.log(numberOfWords)
+    res.render('pool', {numberOfWords});
+});
+router.post('/', async (req,res) => {
+    if(req.body.word.length === 5){
+        await prisma.wort.create({
+            data:{
+                wort: req.body.word.toLowerCase()
+            }
+        })
+    }
+    res.redirect('/pool');
 })
 
 export = router;
